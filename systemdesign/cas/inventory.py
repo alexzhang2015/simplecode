b350a4d11c9273a84aca710d94bb6c751bbd68e0
Alex Zhang <alexzhang2014@live.com>
2023-11-12 20:18:01 +0800

    1112

--- systemdesign/cas/inventory.py
+++ systemdesign/cas/inventory.py
@@ -5,33 +5,34 @@ from inventory_model import Inventory
 
 # 同样的CAS逻辑代码
 
-def reduce_cas(session: Session, product_id:int, num:int) -> bool:
+
+def reduce_cas(session: Session, product_id: int, num: int) -> bool:
     try:
-       
+
         # 获取锁定记录
         inventory = session.query(Inventory)\
-                .filter(Inventory.product_id==product_id)\
-                .with_for_update()\
-                .first()
+            .filter(Inventory.product_id == product_id)\
+            .with_for_update()\
+            .first()
 
-        # 读取库存值  
+        # 读取库存值
         old_total = inventory.total
-        
+
         # 判断库存是否足够
         if old_total < num:
             return False
-        
+
         # CAS更新
         updated = session.query(Inventory) \
             .filter(Inventory.total == old_total, Inventory.product_id == product_id) \
-            .update({Inventory.total: old_total - num}) 
+            .update({Inventory.total: old_total - num})
 
         if updated > 0:
             session.commit()
             return True
-            
-        session.rollback()  
+
+        session.rollback()
         return False
-    
+
     except InvalidStateError:
         pass
--- systemdesign/cas/test.py
+++ systemdesign/cas/test.py
@@ -1,27 +1,27 @@
 from asyncio import InvalidStateError
 from concurrent.futures import ThreadPoolExecutor
 
-from database import init_db, Session, engine  
+from database import init_db, Session, engine
 from inventory import reduce_cas
 from inventory_model import Inventory
 
 init_db()
 
+
 def test(product_id, num):
-    session = Session(bind=engine) 
+    session = Session(bind=engine)
     reduce_cas(session, product_id, num)
     session.close()
 
-# with Session(bind=engine) as session:
-#    test(session, 1, 10)
 
-# 启动线程池测试    
+# 启动线程池测试
 with ThreadPoolExecutor() as executor:
     futures = executor.map(test, [1]*10, [10]*10)
     try:
-        for _ in futures: pass
+        for _ in futures:
+            pass
     except InvalidStateError:
-        pass 
-   
+        pass
+
 with Session(bind=engine) as session:
-   print(session.query(Inventory).filter(Inventory.product_id==1).first().total)
+   print(session.query(Inventory).filter(Inventory.product_id == 1).first().total)
